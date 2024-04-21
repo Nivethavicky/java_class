2be01e520fb8cfdbeaa163f6c4c2d891b7856aff
Nivethavicky <167093019+Nivethavicky@users.noreply.github.com>
2024-04-21 12:13:57 +0530

    Update Break_continue.java

--- Break_continue.java
+++ Break_continue.java
@@ -7,29 +7,24 @@ public class Break_continue {
 		{
 			if(i==5) 
 			{
-				break;   //break in iterative loop
+			 break;   //break in iterative loop
 			}
 			System.out.println("hi"+i);
 			
 		}
-		
-		
-		
-		System.out.println(";;;;;;;;;;;;;;;;;;;;;;;;;;");
+		System.out.println(".......................................");
 			
-//		continue
+	//continue
 		
 		for(int i=0;i<=10;i++)
 		{
 			if(i==5) 
 			{
-				continue;   //break in iterative loop
+		         continue;   //break in iterative loop
 			}
 			System.out.println("hi"+i);
 			
-		}
-		
-		
+		}	
 		
 	}
 }
