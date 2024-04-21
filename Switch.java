110f5005e5039c65acd1837fe2480a38e7d86515
Nivethavicky <167093019+Nivethavicky@users.noreply.github.com>
2024-04-21 12:35:38 +0530

    Update Switch.java

--- Switch.java
+++ Switch.java
@@ -2,29 +2,33 @@ package javaproject;
 
 public class Switch {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		int a=3;
 		int b=4;
 		int res=a*b;
-		switch(res) {
+		switch(res)
+	       {
+				
 		case 1:
-			  res=a+b;
+	        res=a+b;
 	        break;
+				
 		case 2:
-			 res=a-b;
+	        res=a-b;
 	        break;
+			       
 		case 3 :
-			 res=a*b;
+		res=a*b;
 	        break;  
 	        
 		case 4:
-			 res=a/b;
+	        res=a/b;
 	        break;
-	    default:
+			       
+	        default:
 	    	System.out.println("invalid");
-		    break;
-		}
-
+		break;
+	       }
 	}
-
-}
+    }
